3fab42a73783ee643d6bf7bb59e79c928e03c76c
sina <sinadsina78@gmail.com>
2023-11-02 22:58:13 +0330

    update ver 1.0.02
    - complete the homepage of APP

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,37 +4,58 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className="bg-white text-black p-4 flex flex-col justify-center items-center absolute bottom-0 w-full ">
-    <div className="flex flex-wrap container">
-      <div className="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4 sm:mb-0">
-        {/* First Column */}
-        <h2 className="text-lg font-semibold mb-2">Column 1</h2>
-        <p>Content for the first column.</p>
+    <div className="flex flex-wrap container my-5 sm:justify-between">
+      <div className="w-full sm:w-1/5 md:w-1/5 lg:w-1/5 xl:w-1/5 mb-4 sm:mb-0">
+        <div className="flex items-center">
+            <img src="./image/logo.png" alt="Logo" className="w-13 h-13 mr-2" />
+            <h1 className="text-black font-semibold text-xs">Website Name</h1>
+        </div>
       </div>
 
-      <div className="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4 sm:mb-0">
-        {/* Second Column */}
-        <h2 className="text-lg font-semibold mb-2">Column 2</h2>
-        <p>Content for the second column.</p>
+      <div className="w-full sm:w-2/4 md:w-2/4 lg:w-2/4 xl:w-2/4 mb-4 sm:mb-0 sm:pr-10">
+        <p className=' text-[13px] leading-normal '>Lorem ipsum dolor sit amet consectetur.
+             Volutpat quam tortor nunc at aliquet. Sed nulla maecenas adipiscing proin.
+              In magna fames faucibus quis laoreet vitae eu mi. Egestas elit nunc iaculis elementum sed leo aliquet. Vel ac mattis cras cursus aliquam semper.</p>
       </div>
 
-      <div className="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
-        {/* Third Column */}
-        <h2 className="text-lg font-semibold mb-2">Column 3</h2>
-        <p>Content for the third column.</p>
+      <div className="w-full sm:w-1/5 md:w-1/5 lg:w-1/5 xl:w-1/5">
+        <p className=' text-[13px] leading-normal '>Lorem ipsum dolor sit amet consectetur. Volutpat quam tortor nunc at aliquet. Sed nulla maecenas adipiscing proin. In magna fames as </p>
       </div>
     </div>
 
-    <div className="flex flex-wrap mt-4 container ">
-      <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 sm:mb-0">
-        {/* Fourth Column (Second Row, First Column) */}
-        <h2 className="text-lg font-semibold mb-2">Column 4</h2>
-        <p>Content for the fourth column.</p>
+    <div className="flex flex-wrap mt-10 mb-6 container justify-between">
+      <div className="w-full sm:w-2/3 md:w-2/3 lg:w-2/3 xl:w-2/3 mb-4 sm:mb-0">
+        <ul className={`flex sm:space-x-5 items-center text-[13px] ${styles.navbar}`}>
+            <li className="text-black active border-r-2 pr-5">
+            <a href="#">website@2023</a>
+            </li>
+            <li className="text-black border-r-2 pr-5">
+            <a href="#">Term Of Services</a>
+            </li>
+            <li className="text-black border-r-2 pr-5">
+            <a href="#">Sitemap</a>
+            </li>
+            <li className="text-black border-r-2 pr-5">
+            <a href="#">Contact</a>
+            </li>
+            <li className="text-black">
+            <a href="#">9anime</a>
+            </li>
+        </ul>
       </div>
 
-      <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
-        {/* Fifth Column (Second Row, Second Column) */}
-        <h2 className="text-lg font-semibold mb-2">Column 5</h2>
-        <p>Content for the fifth column.</p>
+      <div className="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
+      <ul className={`flex sm:space-x-5 items-center text-[13px] justify-end mr-5`}>
+            <li className="text-black space-x-5">
+            <a href="#"><img src="./image/instagram.png" alt="instagram" /></a>
+            </li>
+            <li className="text-black space-x-5">
+            <a href="#"><img src="./image/facebook.png" alt="facebook" /></a>
+            </li>
+            <li className="text-black space-x-5">
+            <a href="#"><img src="./image/linkedin.png" alt="linkedin" /></a>
+            </li>
+        </ul>
       </div>
     </div>
   </footer>
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -6,22 +6,22 @@ export default function NavBar() {
     <div className="bg-[#272626] p-4">
     <div className="container mx-auto flex justify-between items-center">
       <div className="flex items-center">
-        <img src="./image/logo.png" alt="Logo" className="w-10 h-10 mr-2" />
-        <h1 className="text-white text-2xl font-bold">Your Title</h1>
+        <img src="./image/logo.png" alt="Logo" className="w-12 h-12 mr-2" />
+        <h1 className="text-white font-semibold text-xs">Website Name</h1>
       </div>
 
       <ul className={`flex space-x-10 items-center ${styles.navbar}`}>
         <li className="text-white active">
-          <a href="#">Item 1</a>
+          <a href="#">Home</a>
         </li>
         <li className="text-white">
-          <a href="#">Item 2</a>
+          <a href="#">Movies</a>
         </li>
         <li className="text-white">
-          <a href="#">Item 3</a>
+          <a href="#">TV Shows</a>
         </li>
         <li className="text-white">
-          <a href="#">Item 4</a>
+          <a href="#">Tops IMDB</a>
         </li>
         <li className={`text-white bg-blue-600 px-5 py-2 rounded-full text-sm ${styles.noHover}`}>
           <a className='flex flex-row' href="#">
